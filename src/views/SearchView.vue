861ba8bbb4711d08f7abf66de439ed7fba332f9c
Rodrigo Kalil <rkbuarque@gmail.com>
2024-11-21 14:23:25 -0300

    feat: change search view visuals

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import FooterComponent from '../components/FooterComponent.vue'
 import { useGithubStore, useGithubReposStore } from '../stores/github'
 import { useRouter } from 'vue-router'
 import { onMounted } from 'vue'
@@ -22,7 +23,7 @@ const handleNewSearch = () => {
 </script>
 
 <template>
-  <div class="min-h-screen bg-white p-8">
+  <div class="min-h-screen bg-gradient-to-br from-white to-stone-100 p-8">
     <div class="max-w-6xl mx-auto">
       <div class="flex justify-between items-center mb-12">
         <img src="/src/assets/logo.png" alt="Bitwise" class="h-8" />
@@ -31,36 +32,36 @@ const handleNewSearch = () => {
           <input
             type="text"
             placeholder="Buscar usuário"
-            class="w-96 p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-purple-800"
+            class="shadow-lg w-96 p-2 border border-gray-200 rounded-sm focus:outline-none focus:border-fuchsia-900"
             @keyup.enter="handleNewSearch"
           />
           <button
             @click="handleNewSearch"
-            class="bg-purple-800 text-white px-4 py-2 rounded-lg hover:bg-purple-900"
+            class="bg-fuchsia-950 text-white px-4 py-2 shadow-lg rounded-sm hover:bg-fuchsia-900"
           >
             Buscar
           </button>
         </div>
 
         <div class="flex gap-4 pt-6">
-          <a href="#" class="text-gray-600 hover:text-purple-800"
+          <a href="#" class="text-gray-600 hover:text-fuchsia-900"
             ><img src="/src/assets/medium.png" alt="M" class="h-6 mb-6"
           /></a>
-          <a href="#" class="text-gray-600 hover:text-purple-800"
+          <a href="#" class="text-gray-600 hover:text-fuchsia-900"
             ><img src="/src/assets/linkedin.png" alt="in" class="h-6 mb-6"
           /></a>
-          <a href="#" class="text-gray-600 hover:text-purple-800"
+          <a href="#" class="text-gray-600 hover:text-fuchsia-900"
             ><img src="/src/assets/instagram.png" alt="ig" class="h-6 mb-6"
           /></a>
         </div>
       </div>
 
-      <div v-if="githubStore.user" class="bg-white rounded-lg shadow-lg p-8">
+      <div v-if="githubStore.user" class="bg-white rounded-sm shadow-lg p-8">
         <div class="flex gap-8 mb-8">
           <img
             :src="githubStore.user.avatar_url"
             :alt="githubStore.user.name"
-            class="w-24 h-24 rounded-full"
+            class="w-24 h-24 rounded-sm"
           />
           <div>
             <h2 class="text-2xl font-bold">{{ githubStore.user.name }}</h2>
@@ -89,4 +90,5 @@ const handleNewSearch = () => {
       </div>
     </div>
   </div>
+  <FooterComponent />
 </template>
