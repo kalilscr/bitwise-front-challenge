f5b3ce61c31b2135f9af2c04d240ce703c02e3c1
Rodrigo Kalil <rkbuarque@gmail.com>
2024-11-21 09:14:18 -0300

    feat: update github store

--- src/stores/github.ts
+++ src/stores/github.ts
@@ -17,12 +17,21 @@ export const useGithubStore = defineStore('github', () => {
   async function searchUser(username: string) {
     const alertStore = useAlertStore()
     loading.value = true
+
+    const headers = {
+      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+    }
+
     try {
-      const response = await axios.get(`https://api.github.com/users/${username}`)
+      const response = await axios.get(`https://api.github.com/users/${username}`, {
+        headers: headers,
+      })
+
       user.value = response.data
     } catch (error) {
       user.value = null
       alertStore.showAlert('Nenhum usuário encontrado!')
+      console.error(error)
     } finally {
       loading.value = false
     }
@@ -31,6 +40,80 @@ export const useGithubStore = defineStore('github', () => {
   return {
     user,
     loading,
-    searchUser
+    searchUser,
+  }
+})
+
+export const useGithubReposStore = defineStore('repositories', () => {
+  const repos = ref<unknown | null>(null)
+  const loading = ref(false)
+
+  async function searchRepos(username: string) {
+    const alertStore = useAlertStore()
+    loading.value = true
+
+    const headers = {
+      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+    }
+
+    try {
+      const repositories = await axios.get(
+        `https://api.github.com/search/repositories?q=user:${username}&sort=updated&per_page=10`,
+        {
+          headers: headers,
+        },
+      )
+
+      const repoNames = repositories.data.items.map((item) => item.name)
+      //const totalPublicRepos = repositories.data.total_count
+
+      const repositoriesStats = []
+
+      for (const repoName of repoNames) {
+        const repositoriesData = await axios.get(
+          `https://api.github.com/repos/${username}/${repoName}/commits?per_page=1&page=1`,
+          {
+            headers: headers,
+          },
+        )
+
+        const commitsCountPerRepo = repositoriesData.headers.link.split(',')[1].split(/[=\>]/)[2] // catch second element of first split and then split between = and > and catch the third element
+
+        const result = repositoriesData.data.map((item) => {
+          const message = item.commit.message
+          const hash = item.sha
+          return { hash: hash, message: message }
+        })
+
+        const data = result.reduce((acc, curr) => {
+          acc = {
+            hash: curr.hash,
+            message: curr.message,
+          }
+          return acc
+        }, {})
+
+        repositoriesStats.push({
+          name: repoName,
+          qtdCommits: commitsCountPerRepo,
+          hash: data.hash,
+          message: data.message,
+        })
+      }
+
+      repos.value = repositoriesStats
+    } catch (error) {
+      repos.value = null
+      alertStore.showAlert('Nenhum repositório encontrado!')
+      console.error(error)
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return {
+    repos,
+    loading,
+    searchRepos,
   }
 })
