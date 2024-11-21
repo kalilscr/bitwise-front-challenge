92ef9e2958f1706a713e82fc762f3eaee25ca8d2
Rodrigo Kalil <rkbuarque@gmail.com>
2024-11-21 14:26:54 -0300

    feat: change home view visuals

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,8 +11,9 @@ const username = ref('')
 const handleSearch = async () => {
   if (username.value) {
     await githubStore.searchUser(username.value)
-    await reposStore.searchRepos(username.value)
-    if (githubStore.user && reposStore.repos) {
+    // todo: refactor this
+    if (githubStore.user && !githubStore.user.public_repos) {
+      await reposStore.searchRepos(username.value)
       router.push('/search')
     }
   }
@@ -20,15 +21,17 @@ const handleSearch = async () => {
 </script>
 
 <template>
-  <div class="min-h-screen bg-gradient-to-br from-white to-purple-100 p-8">
+  <div class="min-h-screen bg-gradient-to-br from-white to-stone-100 p-8">
     <div class="max-w-4xl mx-auto">
       <img src="/src/assets/logo.png" alt="Bitwise" class="h-12 mb-12" />
 
       <div class="mb-12">
-        <h1 class="text-4xl font-bold mb-2">
-          Buscar um usuário no <span class="text-purple-800">Github</span>
+        <h1 class="text-4xl text-fuchsia-950 mb-2">
+          Buscar um usuário no <span class="font-bold text-fuchsia-950">Github</span>
         </h1>
-        <p class="text-2xl">ficou muito <span class="text-purple-800">fácil!</span></p>
+        <p class="text-4xl text-fuchsia-950">
+          ficou muito <span class="font-bold text-fuchsia-950">fácil!</span>
+        </p>
       </div>
 
       <div class="flex gap-2">
@@ -36,12 +39,12 @@ const handleSearch = async () => {
           v-model="username"
           type="text"
           placeholder="Buscar usuário"
-          class="flex-1 p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-purple-800"
+          class="shadow-lg max-w-md flex-1 p-2 border border-gray-200 rounded-sm focus:outline-none focus:border-fuchsia-900"
           @keyup.enter="handleSearch"
         />
         <button
           @click="handleSearch"
-          class="bg-purple-800 text-white px-4 py-2 rounded-lg hover:bg-purple-900"
+          class="bg-fuchsia-950 text-white px-4 py-2 shadow-lg rounded-sm hover:bg-fuchsia-900"
         >
           Buscar
         </button>
@@ -50,13 +53,13 @@ const handleSearch = async () => {
       <div
         class="flex gap-4 mt-12 fixed bottom-0 left-0 z-20 w-full p-4 md:flex md:items-center md:justify-center md:p-6"
       >
-        <a href="#" class="text-gray-600 hover:text-purple-800"
+        <a href="#" class="text-gray-600 hover:text-fuchsia-900"
           ><img src="/src/assets/medium.png" alt="M" class="h-6 mb-6"
         /></a>
-        <a href="#" class="text-gray-600 hover:text-purple-800"
+        <a href="#" class="text-gray-600 hover:text-fuchsia-900"
           ><img src="/src/assets/linkedin.png" alt="in" class="h-6 mb-6"
         /></a>
-        <a href="#" class="text-gray-600 hover:text-purple-800"
+        <a href="#" class="text-gray-600 hover:text-fuchsia-900"
           ><img src="/src/assets/instagram.png" alt="ig" class="h-6 mb-6"
         /></a>
       </div>
