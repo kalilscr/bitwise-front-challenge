c70058f098aa383a01f0dce83a573215962dd600
Rodrigo Kalil <rkbuarque@gmail.com>
2024-11-22 12:47:14 -0300

    feat: add loading button for home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,14 +7,22 @@ const router = useRouter()
 const githubStore = useGithubStore()
 const reposStore = useGithubReposStore()
 const username = ref('')
+const isLoading = ref(false)
 
 const handleSearch = async () => {
   if (username.value) {
+    isLoading.value = true
+
     await githubStore.searchUser(username.value)
-    // todo: refactor this
-    if (githubStore.user && !githubStore.user.public_repos) {
+
+    isLoading.value = false
+    if (githubStore.user) {
+      isLoading.value = true
+
       await reposStore.searchRepos(username.value)
       router.push('/search')
+
+      isLoading.value = false
     }
   }
 }
@@ -25,7 +33,7 @@ const handleSearch = async () => {
     <div class="max-w-4xl mx-auto">
       <img src="/src/assets/logo.png" alt="Bitwise" class="h-12 mb-12" />
 
-      <div class="mb-12">
+      <div class="mb-20 mt-24">
         <h1 class="text-4xl text-fuchsia-950 mb-2">
           Buscar um usuário no <span class="font-bold text-fuchsia-950">Github</span>
         </h1>
@@ -44,9 +52,32 @@ const handleSearch = async () => {
         />
         <button
           @click="handleSearch"
-          class="bg-fuchsia-950 text-white px-4 py-2 shadow-lg rounded-sm hover:bg-fuchsia-900"
+          class="bg-fuchsia-950 text-white px-4 py-2 shadow-lg rounded-sm hover:bg-fuchsia-900 flex items-center"
+          :disabled="isLoading"
         >
-          Buscar
+          <template v-if="isLoading">
+            <svg
+              class="animate-spin h-6 w-8 mx-1.5 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                class="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+              ></circle>
+              <path
+                class="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          </template>
+          <template v-else> Buscar </template>
         </button>
       </div>
 
