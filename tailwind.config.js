5db16bdf84c3f0874c58ec7c197d7353020658f5
Rodrigo Kalil <rkbuarque@gmail.com>
2024-11-22 12:53:17 -0300

    raw: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      maxHeight: {
+        112: '28rem',
+        120: '30rem',
+        128: '32rem',
+      },
+    },
   },
   plugins: [],
 }
